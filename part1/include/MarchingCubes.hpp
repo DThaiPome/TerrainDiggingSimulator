af8a589f2fdfbd43e07d1beb3622283f15302c40
DThaiPome <r.gehrlein154@gmail.com>
2021-12-10 13:27:15 -0500

    lol this is awful

---
+++ part1/include/MarchingCubes.hpp
@@ -0,0 +1,42 @@
+#ifndef MARCHING_CUBES_HPP
+#define MARCHING_CUBES_HPP
+
+#include "Object.hpp"
+#include <vector>
+
+class MarchingCubes : public Object { 
+public:
+    MarchingCubes(unsigned int xSegs, unsigned int ySegs, unsigned int zSegs, float xDim, float yDim, float zDim, float* data, float surfaceLevel);
+    ~MarchingCubes();
+private:
+    struct Vector3 {
+        float x;
+        float y;
+        float z;
+
+        Vector3 operator+(const Vector3 & other) const {
+            return {x + other.x, y + other.y, z + other.z}; 
+        }
+    };
+
+    struct Vertex {
+        Vector3 position;
+        Vector3 normal;
+        unsigned int usageCount;
+        unsigned int index;
+    };
+
+    struct MeshData {
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+    };
+
+    MeshData march_cubes(unsigned int xSegs, unsigned int ySegs, unsigned int zSegs, float xUnit, float yUnit, float zUnit, float* data, float surfaceLevel);
+
+    void fill_triangulations(const std::vector<std::pair<Vector3, Vector3>> & triangulation, std::map<std::pair<Vector3, Vector3>, MarchingCubes::Vertex> & vertexMap, std::vector<unsigned int> indices, float* data, float surfaceLevel, float xSegs, float ySegs, float zSegs, float xUnit, float yUnit, float zUnit, unsigned int & globalIndex);
+
+    Vector3 point_pair_to_position(std::pair<Vector3, Vector3> positionPair, float valueA, float valueB, float surfaceLevel, float xUnit, float yUnit, float zUnit);
+
+    Vector3 triangle_normal(Vector3* positions);
+};
+#endif
